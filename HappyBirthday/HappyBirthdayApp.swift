997076e3ff1634fee606b6faf6bed2c04ed22c2c
wx <Azure.cwx@gmail.com>
2022-04-09 18:28:46 +0800

    Initial Commit

---
+++ HappyBirthday/HappyBirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyBirthdayApp.swift
+//  HappyBirthday
+//
+//  Created by Hidroxide on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct HappyBirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
